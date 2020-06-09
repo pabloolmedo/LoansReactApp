76db144998877d112c93f721305a7767b59253d0
Pablo Olmedo <pablo.olmedo@cba.gob.ar>
2020-06-09 23:38:06 +1000

    Updates

---
+++ precache-manifest.ac1e3273fc48141eaf9c0be4907cebfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe75046f20bdcbd2b8e59a11337f54e",
+    "url": "/pabloolmedo/LoansReactApp/index.html"
+  },
+  {
+    "revision": "3ac0a66204bef8cf74dc",
+    "url": "/pabloolmedo/LoansReactApp/static/css/main.2f941c37.chunk.css"
+  },
+  {
+    "revision": "e2a849bdb4e6839546da",
+    "url": "/pabloolmedo/LoansReactApp/static/js/2.c63989ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pabloolmedo/LoansReactApp/static/js/2.c63989ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac0a66204bef8cf74dc",
+    "url": "/pabloolmedo/LoansReactApp/static/js/main.4ec2dcdf.chunk.js"
+  },
+  {
+    "revision": "3b9abbcf59b5ef8a2c17",
+    "url": "/pabloolmedo/LoansReactApp/static/js/runtime-main.8e9f4cd8.js"
+  }
+]);
